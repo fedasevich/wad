64f6f3538bce1dcb9ce060c2b340b6d2993fa28c
Fedasevich <yurafedasevka@gmail.com>
2023-05-09 14:10:54 +0200

    added showing of selected currency by default

--- client/src/components/Currencies.js
+++ client/src/components/Currencies.js
@@ -1,59 +1,62 @@
 import { observer } from 'mobx-react-lite'
-import React from 'react'
-import { mainCurrencies,cryptoCurrencies } from '../utils/consts'
+import React, { useState } from 'react'
+import { mainCurrencies, cryptoCurrencies } from '../utils/consts'
 
 
-const Currencies = observer(({setCurrency}) => {
- 
+const Currencies = observer(({ setCurrency, walletDefaultCurrency }) => {
+const [activeCurrency, setActiveCurrency] = useState(walletDefaultCurrency)
 
 
-const handleChange= (event)=> {
-  setCurrency(event)
-}
+  const handleChange = (event) => {
+    setCurrency(event)
+    setActiveCurrency(event.target.value)
+  }
+
 
   return (
     <>
 
-  <div> 
-  <h6>{mainCurrencies.name}</h6>
- {mainCurrencies.data.map(currency=>{
-  
-    return (
-    <div className='d-flex flex-row mb-2' key={currency.symbol}> 
+      <div>
+        <h6>{mainCurrencies.name}</h6>
+        {mainCurrencies.data.map(currency => {
+          return (
+            <div className='d-flex flex-row mb-2' key={currency.symbol}>
+              <input
+              type="radio"
+              id={currency.currency}
+              name="currency"
+              value={currency.symbol}
+              checked={currency.symbol === walletDefaultCurrency}
+              onChange={(e) => { handleChange(e) }}
+              />
+              <label className='ms-2 ' htmlFor={currency.currency}> {currency.currency}  </label>
+              <p className='mb-0 ms-auto'>{currency.symbol}</p>
 
-<input type="radio" id={currency.currency} name="currency" value={currency.symbol} 
+            </div>
+          )
+        })
+        }
+      </div>
 
+      <div>
+        <h6>{cryptoCurrencies.name}</h6>
+        {cryptoCurrencies.data.map(currency => {
 
-onChange={(e)=>{ handleChange(e)}} />
-<label className='ms-2 ' htmlFor={currency.currency}> {currency.currency}  </label>
-<p className='mb-0 ms-auto'>{currency.symbol}</p>
+          return (
+            <div className='d-flex flex-row mb-2' key={currency.symbol}>
 
-    </div>  
-    )
-   
-    })
- }
-</div> 
+              <input checked={currency.symbol === walletDefaultCurrency} type="radio" id={currency.currency} name="currency" value={currency.symbol} onChange={(e) => { handleChange(e) }} />
+              <label className='ms-2 ' htmlFor={currency.currency}> {currency.currency}  </label>
+              <p className='mb-0 ms-auto'>{currency.symbol}</p>
 
-<div> 
-  <h6>{cryptoCurrencies.name}</h6>
- {cryptoCurrencies.data.map(currency=>{
-  
-    return (
-        <div className='d-flex flex-row mb-2'  key={currency.symbol}>  
+            </div>
+          )
 
-<input type="radio" id={currency.currency} name="currency" value={currency.symbol} onChange={(e)=>{ handleChange(e)}}/>
-<label className='ms-2 ' htmlFor={currency.currency}> {currency.currency}  </label>
-<p className='mb-0 ms-auto'>{currency.symbol}</p>
+        })
+        }
+      </div>
 
-    </div>  
-    )
-   
-    })
- }
-</div> 
-
-</> 
+    </>
   )
 })
 
--- client/src/components/wallet/EditWallet.js
+++ client/src/components/wallet/EditWallet.js
@@ -1,8 +1,8 @@
-import React, { useContext, useState, } from 'react'
+import React, { useContext, useEffect, useState, } from 'react'
 import { Context } from '../..'
 
 
-import {DispatchContext} from '../../pages/MainPage';
+import { DispatchContext } from '../../pages/MainPage';
 import { DeleteIcon } from '../../ui/Icons/ControlIcons/ControlIcons';
 import Currencies from '../Currencies';
 import MenuProvider from '../MenuProvider'
@@ -10,85 +10,90 @@ import MenuProvider from '../MenuProvider'
 
 
 
-const EditWallet = ({id}) => {
+const EditWallet = ({ id }) => {
+  const { wallet } = useContext(Context)
+  const dispatch = useContext(DispatchContext)
   const [editWallet, setEditWallet] = useState({
     name: "",
     currency: "",
-    balance:"",
+    balance: "",
   })
-  const { wallet } = useContext(Context)
-  const dispatch = useContext(DispatchContext)
 
 
-const eraseState=()=>{
-  setEditWallet({ name: "",
-  currency: "",
-  balance:"",})
-}
-
-const handleClose = ()=> {
-
-  dispatch({ operation: "DEFAULT_WALLET", id:-1 })
-}
-
-
-
-const handleCommit = ()=> {
-  wallet.editWallet(id,editWallet.currency,editWallet.name,editWallet.balance)
-  eraseState()
-  handleClose()
-}
-
-const handleKeyDown = event => {
-  if (event.key === 'Enter') {
-    handleCommit()
+  const eraseState = () => {
+    setEditWallet({
+      name: "",
+      currency: "",
+      balance: "",
+    })
   }
-  if (event.key === 'Escape') {
+
+  const handleClose = () => {
+
+    dispatch({ operation: "DEFAULT_WALLET", id: -1 })
+  }
+
+  useEffect(() => {
+    eraseState()
+  }, [id])
+
+  const handleCommit = () => {
+    wallet.editWallet(id, editWallet.currency, editWallet.name, editWallet.balance)
+    eraseState()
     handleClose()
   }
-};
 
-const handleChange=(event)=> {
-  setEditWallet({
-    ...editWallet,
-    [event.target.name]: event.target.value
-  });
- 
-}
+  const handleKeyDown = event => {
+    if (event.key === 'Enter') {
+      handleCommit()
+    }
+    if (event.key === 'Escape') {
+      handleClose()
+    }
+  };
 
-const handleDoubleClick=(id)=> {
+  const handleChange = (event) => {
+    console.log(event.target.name)
+    setEditWallet({
+      ...editWallet,
+      [event.target.name]: event.target.value
+    });
 
-  wallet.deleteWallet(id)
-  eraseState()
-  handleClose()
-}
+  }
+
+  const handleDoubleClick = (id) => {
+
+    wallet.deleteWallet(id)
+    eraseState()
+    handleClose()
+  }
 
   return (
     <>
-  <MenuProvider>
-    <MenuProvider.Actions close={handleClose} commit={handleCommit}>
-      <h5>Edit wallet</h5> 
-      <h6>Wallet: {wallet.getWalletById(id).name}</h6>
-      </MenuProvider.Actions>
-    
-    <MenuProvider.Container className="d-flex flex-column">
-      <label className='mb-2' htmlFor="name">Enter new name:</label>
-      <input className='mb-2 component-half-border-radius' type="text" name='name' onKeyDown={handleKeyDown} value={editWallet.name} onChange={handleChange}/>
+      <MenuProvider>
+        <MenuProvider.Actions close={handleClose} commit={handleCommit}>
+          <h5>Edit wallet</h5>
+          <h6>Wallet: {wallet.getWalletById(id).name}</h6>
+        </MenuProvider.Actions>
+
+        <MenuProvider.Container className="d-flex flex-column">
+          <label className='mb-2' htmlFor="name">Enter new name:</label>
+          <input className='mb-2 component-half-border-radius' type="text" name='name' onKeyDown={handleKeyDown} value={editWallet.name} onChange={handleChange} />
 
 
-      <label className='mb-2' htmlFor="balance">Enter new balance:</label>
-      <input className='mb-2 component-half-border-radius' type="number" name='balance' onKeyDown={handleKeyDown} value={editWallet.balance} onChange={handleChange}/>
-      <h4 className='mb-2' >Choose new currency:</h4>
-   <Currencies setCurrency={handleChange} />
+          <label className='mb-2' htmlFor="balance">Enter new balance:</label>
+          <input className='mb-2 component-half-border-radius' type="number" name='balance' onKeyDown={handleKeyDown} value={editWallet.balance} onChange={handleChange} />
+          <h4 className='mb-2' >Choose new currency:</h4>
+          <Currencies setCurrency={handleChange} walletDefaultCurrency={wallet.getWalletById(id).currency} />
 
-   <h6 onDoubleClick={()=>handleDoubleClick(id)} className='text-danger mb-0 btn' ><DeleteIcon/> Delete wallet</h6>
-       </MenuProvider.Container>
-  </MenuProvider> 
-  </>
+          <h6 onDoubleClick={() => handleDoubleClick(id)} className='text-danger mb-0 btn' ><DeleteIcon /> Delete wallet</h6>
+        </MenuProvider.Container>
+      </MenuProvider>
+    </>
   )
 
-   
-    
+
+
 
 }
 
