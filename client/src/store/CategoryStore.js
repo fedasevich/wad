b2ef5dea5043f6db3196978ecdb775f417992085
Fedasevich <yurafedasevka@gmail.com>
2023-07-14 19:19:22 +0200

    * parced categories

--- client/src/store/CategoryStore.js
+++ client/src/store/CategoryStore.js
@@ -1,255 +1,260 @@
-import {makeAutoObservable, runInAction} from "mobx";
+import { makeAutoObservable, runInAction } from "mobx";
 import { changeCategory, createCategory, deleteCategory, fetchCategory, fetchCategoryPeriod } from "../http/categoryApi";
 
 import { changeTransaction, createTransaction, deleteTransaction } from "../http/transactionApi";
 
 
 export default class CategoryStore {
-    constructor() {
-        this._categories = [
+  constructor() {
+    this._categories = []
+    this._parsedCategories = []
+    this._transactions = []
+    this._transactionsLimit = 5
+    this._transactionsPage = 1
+    this._transactionsSort = "DESC"
+    this._selectedCategory = {}
+    this._selectedTransaction = {}
+    makeAutoObservable(this)
+  }
 
-        ]
-        this._transactions = [
-           
-        ]
-        this._transactionsLimit = 5
-        this._transactionsPage = 1
-        this._transactionsSort = "DESC"
-        this._selectedCategory = {}
-        this._selectedTransaction = {}
-        makeAutoObservable(this)
-    }
+  setCategories(categories) {
+    this._categories = categories
+  }
 
-    setCategories(categories) {
-        this._categories = categories
-    }
+  setParsedCategories(parsedCategories) {
+    this._parsedCategories = parsedCategories
+  }
 
-    setTransactions(transactions) {
-        this._transactions = transactions
-    }
+  setTransactions(transactions) {
+    this._transactions = transactions
+  }
 
-    setSelectedCategory(category) {
-        this._selectedCategory = category
-    }
+  setSelectedCategory(category) {
+    this._selectedCategory = category
+  }
 
-    setSelectedTransaction(transaction) {
-        this._selectedTransaction = transaction
-    }
+  setSelectedTransaction(transaction) {
+    this._selectedTransaction = transaction
+  }
 
-    setTransactionsLimit(limit) {
-        this._transactionsLimit = limit
-    }
+  setTransactionsLimit(limit) {
+    this._transactionsLimit = limit
+  }
 
-    setTransactionsPage(page) {
-        this._transactionsPage = page
-    }
+  setTransactionsPage(page) {
+    this._transactionsPage = page
+  }
 
-    setTransactionsSort(sort) {
-        this._transactionsSort = sort
-    }
+  setTransactionsSort(sort) {
+    this._transactionsSort = sort
+  }
 
-    get categories() {
-        return this._categories
-    }
+  get categories() {
+    return this._categories
+  }
 
-    get selectedTransaction() {
-        return this._selectedTransaction
-    }
-    get transactions() {
-        return this._transactions
-    }
+  get parsedCategories() {
+    return this._parsedCategories
+  }
 
-    get selectedCategory() {
-        return this._selectedCategory
-    }
+  get selectedTransaction() {
+    return this._selectedTransaction
+  }
+  get transactions() {
+    return this._transactions
+  }
 
-    get transactionsPage() {
-        return this._transactionsPage
-    }
+  get selectedCategory() {
+    return this._selectedCategory
+  }
 
-    get transactionsLimit() {
-        return this._transactionsLimit
-    }
+  get transactionsPage() {
+    return this._transactionsPage
+  }
 
-    get transactionsSort() {
-        return this._transactionsSort
-    }
+  get transactionsLimit() {
+    return this._transactionsLimit
+  }
 
-    createTransaction(currentOperand,selectedCategory,selectedWallet,description,wallet) {
-           try {
-        createTransaction(selectedCategory.id,selectedWallet.id, description ?  description:selectedCategory.name , parseFloat(currentOperand)).
-        then(data=> {
-            runInAction(()=>{
-        this.transactions.unshift(data)
-        selectedWallet.balance -= parseFloat(currentOperand)
-        selectedCategory.spent += parseFloat(currentOperand)
-        })
-    })
-      } catch(e) {
-        alert(e.response.data.message);
-      }
-    }
+  get transactionsSort() {
+    return this._transactionsSort
+  }
 
-    deleteTransaction(transactionId,categoryId,walletId,wallet) {    
-        try {
-            deleteTransaction(transactionId).
-            then(()=> {
-                runInAction(()=>{
-                const transaction =  this.getTransactionById(transactionId)
-                const category= this.getCategoryById(categoryId)
-                if(walletId !== -1) 
-               {
-                 const foundWallet = wallet.getWalletById(walletId)
-                 foundWallet.balance += parseFloat(transaction.sum)
-              }
-                category.spent -= parseFloat(transaction.sum)
-                this.transactions.splice(this.transactions.indexOf(transaction),1)
-                  }) 
-            })
-          } catch(e) {
-            alert(e.response.data.message);
-          }
-    }
-
-    getTransactionById=(id)=>{
-        return this.transactions.find((transaction)=> transaction.id === id)
-    }
-
-    getCategoryById=(id) => {
-        if(id===-1) return this.categories[0]
-        return this.categories.find((category)=> category.id === id)
-    }
-
-    getIconIdFromCategoryById =(id)=> {
-        const findCategory = this.getCategoryById(id)
-        return findCategory?.iconId
-      }
-    
-    createCategory(newCategoryName,newCategorySelectedIcon) {
-        try {
-            createCategory(newCategoryName,newCategorySelectedIcon.id).
-            then(data=> {
-              runInAction(() => {
-              this.categories.push(data)
-
-              })
-            })
-          } catch(e) {
-            alert(e.response.data.message);
-          }
-    }
-
-    changeTransaction(id,newSum,newDescription,wallet) {
-        if (!newSum && !newDescription) {
-            return alert(`Not enough data`)
-          }
-          const transaction = this.getTransactionById(id)
-          try {
-            changeTransaction(transaction.id, parseFloat(newSum), newDescription).
-              then(() => {
-                runInAction(() => {
-                    const categoryToChange = this.getCategoryById(transaction.categoryId)
-                    const walletToChange = wallet.getWalletById(transaction.walletId)
-                  if (newSum) {
-                    if (transaction.walletId !== -1) {
-                        walletToChange.balance = (parseFloat(walletToChange.balance) + parseFloat(transaction.sum)) - parseFloat(newSum)
-                    }
-                    if(categoryToChange.id !==-1) {
-                        categoryToChange.spent = (parseFloat(categoryToChange.spent) - parseFloat(transaction.sum)) + parseFloat(newSum)
-                    }
-                    transaction.sum = newSum
-                  }
-                  if (newDescription) {
-                    categoryToChange.description = newDescription
-                    transaction.description = newDescription
-                  }
-                })
-              })
-          } catch (e) {
-            alert(e.response.data.message);
-          }
-    }
-
-    changeCategory(id, editCategory) {
-        if (!editCategory || !editCategory.spent && !editCategory.name && !Object.keys(editCategory.icon).length) {
-            return alert(`Not enough data`)
-          }
-         console.log(editCategory)
-          try {
-            changeCategory(id, parseFloat(editCategory.spent), editCategory.name, editCategory.icon.id).
-              then(() => {
-      
-               
-                runInAction(() => {
-                  const categoryToEdit = this.getCategoryById(id)
-                  if(editCategory.name) categoryToEdit.name = editCategory.name
-                  if(editCategory.spent) categoryToEdit.spent = editCategory.spent
-                  if(editCategory.icon.id) categoryToEdit.iconId = editCategory.icon.id
-            
-                })
-               
-              })
-          } catch (e) {
-            alert(e.response.data.message);
-          }
-    }
-
-    fetchCategoryPeriod(dateRange) {
-      if(!dateRange || !dateRange[0].startDate || !dateRange[0].endDate) return
-      try {
-
-        fetchCategoryPeriod(dateRange[0].startDate.toISOString(), dateRange[0].endDate.toISOString()).then(data => {
-
-          if (!data) {
-            return
-          }
+  createTransaction(currentOperand, selectedCategory, selectedWallet, description, wallet) {
+    try {
+      createTransaction(selectedCategory.id, selectedWallet.id, description ? description : selectedCategory.name, parseFloat(currentOperand))
+        .then(data => {
           runInAction(() => {
-            this.categories.forEach(categoryMap => {
-              categoryMap.spent = 0;
-            })
-            data.rows.forEach(dataMap => {
-              this.categories.map(categoryMap => {
-
-                if (dataMap.categoryId === categoryMap.id) {
-                  categoryMap.spent += parseFloat(dataMap.sum)
-                }
-              }
-              )
-            })
+            this.transactions.unshift(data)
+            selectedWallet.balance -= parseFloat(currentOperand)
+            selectedCategory.spent += parseFloat(currentOperand)
           })
         })
-
-      } catch (e) {
-        alert(e.response.data.message);
-      }
+    } catch (e) {
+      alert(e.response.data.message);
     }
+  }
 
-    fetchCategory() {
-      try {
-        fetchCategory().then(data => {
+  deleteTransaction(transactionId, categoryId, walletId, wallet) {
+    try {
+      deleteTransaction(transactionId)
+        .then(() => {
           runInAction(() => {
+            const transaction = this.getTransactionById(transactionId)
+            const category = this.getCategoryById(categoryId)
+            if (walletId !== -1) {
+              const foundWallet = wallet.getWalletById(walletId)
+              foundWallet.balance += parseFloat(transaction.sum)
+            }
+            category.spent -= parseFloat(transaction.sum)
+            this.transactions.splice(this.transactions.indexOf(transaction), 1)
+          })
+        })
+    } catch (e) {
+      alert(e.response.data.message);
+    }
+  }
+
+  getTransactionById = (id) => {
+    return this.transactions.find((transaction) => transaction.id === id)
+  }
+
+  getCategoryById = (id) => {
+    if (id === -1) return this.parsedCategories[0]
+    return this.parsedCategories.find((category) => category.id === id)
+  }
+
+  getIconIdFromCategoryById = (id) => {
+    const findCategory = this.getCategoryById(id)
+    return findCategory?.iconId
+  }
+
+  createCategory(newCategoryName, newCategorySelectedIcon) {
+    try {
+      return createCategory(newCategoryName, newCategorySelectedIcon.id)
+        .then(data => {
+          runInAction(() => {
+            this.parsedCategories.push(data)
+
+          })
+        })
+    } catch (e) {
+      alert(e.response.data.message);
+    }
+  }
+
+  changeTransaction(id, newSum, newDescription, wallet) {
+    if (!newSum && !newDescription) {
+      return alert(`Not enough data`)
+    }
+    const transaction = this.getTransactionById(id)
+    try {
+      changeTransaction(transaction.id, parseFloat(newSum), newDescription)
+        .then(() => {
+          runInAction(() => {
+            const categoryToChange = this.getCategoryById(transaction.categoryId)
+            const walletToChange = wallet.getWalletById(transaction.walletId)
+            if (newSum) {
+              if (transaction.walletId !== -1) {
+                walletToChange.balance = (parseFloat(walletToChange.balance) + parseFloat(transaction.sum)) - parseFloat(newSum)
+              }
+              if (categoryToChange.id !== -1) {
+                categoryToChange.spent = (parseFloat(categoryToChange.spent) - parseFloat(transaction.sum)) + parseFloat(newSum)
+              }
+              transaction.sum = newSum
+            }
+            if (newDescription) {
+              categoryToChange.description = newDescription
+              transaction.description = newDescription
+            }
+          })
+        })
+    } catch (e) {
+      alert(e.response.data.message);
+    }
+  }
+
+  changeCategory(id, editCategory) {
+    if ((editCategory || !editCategory.spent) && !editCategory.name && !Object.keys(editCategory.icon).length) {
+      return alert(`Not enough data`)
+    }
+    try {
+      return changeCategory(id, parseFloat(editCategory.spent), editCategory.name, editCategory.icon.id)
+        .then((data) => {
+          runInAction(() => {
+            const categoryToEdit = this.getCategoryById(id)
+            Object.assign(categoryToEdit, data)
+          })
+        })
+    } catch (e) {
+      alert(e.response.data.message);
+    }
+  }
+
+  fetchCategoryPeriod(dateRange) {
+    if (!dateRange || !dateRange[0].startDate || !dateRange[0].endDate) return;
+
+    const startDate = dateRange[0].startDate.toISOString();
+    const endDate = dateRange[0].endDate.toISOString();
+
+    fetchCategoryPeriod(startDate, endDate)
+      .then(data => {
+        if (!data) {
+          return;
+        }
+
+        this.parseCategories(data)
+      })
+      .catch(error => {
+        alert(error.message);
+      });
+  }
+
+  parseCategories(data) {
+    runInAction(() => {
+      this._parsedCategories = this._categories.map(category => {
+        const categoryId = category.id;
+        const spent = data.rows.reduce((total, row) => {
+          if (row.categoryId === categoryId) {
+            return total + parseFloat(row.sum);
+          }
+          return total;
+        }, 0);
+
+        return {
+          ...category,
+          spent,
+        };
+      });
+    });
+  }
+
+  fetchCategory() {
+    try {
+      fetchCategory().then(data => {
+        runInAction(() => {
           this.setCategories(data)
         })
       })
-      } catch (e) {
-        alert(e.response.data.message);
-      }
+    } catch (e) {
+      alert(e.response.data.message);
     }
-  
+  }
 
-    deleteCategory(id) {
-      try {
-        deleteCategory(id).
-        then(() => {
+
+  deleteCategory(id) {
+    try {
+      deleteCategory(id)
+        .then(() => {
           runInAction(() => {
-            const categoryIndex = this.categories.findIndex(category => category.id === id)
-            this.categories.splice(categoryIndex, 1)
+            const categoryIndex = this.parsedCategories.findIndex(category => category.id === id)
+            this.parsedCategories.splice(categoryIndex, 1)
           })
 
         })
-      } catch (e) {
-        alert(e.response.data.message);
-      }
+    } catch (e) {
+      alert(e.response.data.message);
     }
+  }
 
 }
