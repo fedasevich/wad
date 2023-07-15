b42eef6fe0fb63accfdeea2a34f644b3dc5a7ca2
Fedasevich <yurafedasevka@gmail.com>
2023-07-15 15:00:21 +0200

    * db ssl options

--- server/db.js
+++ server/db.js
@@ -1,20 +1,23 @@
-const {Sequelize} = require('sequelize')
+const { Sequelize } = require('sequelize')
 require('pg')
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 module.exports = new Sequelize(
-process.env.DB_NAME,
-process.env.DB_USER,
-process.env.DB_PASSWORD,
-{
-    dialect:'postgres',
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    dialect: 'postgres',
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialectOptions: {
-        ssl: {
+      ssl: isProduction
+        ? {
           require: true,
           rejectUnauthorized: false,
-        },
-      },
-}
-
+        }
+        : false,
+    },
+  }
 )
