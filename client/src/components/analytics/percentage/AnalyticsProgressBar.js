bc7e7c530093a20c10afa0a8862c94c7c760cf45
Fedasevich <yurafedasevka@gmail.com>
2023-08-07 20:16:20 +0200

    + percentage

---
+++ client/src/components/analytics/percentage/AnalyticsProgressBar.js
@@ -0,0 +1,11 @@
+import { observer } from 'mobx-react-lite';
+import { getCategoryBackgroundColorByIconId } from '../../../ui/Icons/CategoryIcons/CategoryIcons';
+
+
+export const AnalyticsProgressBar = observer(({ className, max = 100, value = 0, iconId }) => {
+    return (
+        <div className={`progress w-100 ${className}`}>
+            <div className="progress-bar" role="progressbar" style={{ width: `${value}%`, backgroundColor: getCategoryBackgroundColorByIconId(iconId) }} aria-valuenow={value} aria-valuemin="0" aria-valuemax={max}></div>
+        </div>
+    );
+})
