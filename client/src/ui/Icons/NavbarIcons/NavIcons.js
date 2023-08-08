ec9b0d303cad6199f861f566e299de7049fa48eb
Fedasevich <yurafedasevka@gmail.com>
2023-08-08 11:27:32 +0200

    * icons code

--- client/src/ui/Icons/NavbarIcons/NavIcons.js
+++ client/src/ui/Icons/NavbarIcons/NavIcons.js
@@ -1,51 +1,28 @@
-import {ReactComponent as Logo} from './logo.svg'
-import {ReactComponent as ChartBar} from './ChartBar.svg'
-import {ReactComponent as ChartPie} from './ChartPie.svg'
-import {ReactComponent as ClipboardText} from './ClipboardText.svg'
-import {ReactComponent as CreditCard} from './CreditCard.svg'
-import {ReactComponent as GearSix} from './GearSix.svg'
-import {ReactComponent as SignOut} from './SignOut.svg'
+import { ReactComponent as ChartBar } from './ChartBar.svg'
+import { ReactComponent as ChartPie } from './ChartPie.svg'
+import { ReactComponent as ClipboardText } from './ClipboardText.svg'
+import { ReactComponent as CreditCard } from './CreditCard.svg'
+import { ReactComponent as GearSix } from './GearSix.svg'
+import { ReactComponent as SignOut } from './SignOut.svg'
+import { ReactComponent as Logo } from './logo.svg'
 
 
-export const LogoIcon = () =>{
-    return(
-      <Logo/>  
-    )
-  }
+export const LogoIcon = () => <Logo />
 
-  
-export const ChartBarIcon = () =>{
-    return(
-      <ChartBar/>  
-    )
-  }
-  
-export const ChartPieIcon = () =>{
-    return(
-      <ChartPie/>  
-    )
-  }
-  
-export const ClipboardTextIcon = () =>{
-    return(
-      <ClipboardText/>  
-    )
-  }
 
-  
-export const CreditCardIcon = () =>{
-    return(
-      <CreditCard/>  
-    )
-  }
+export const ChartBarIcon = () => <ChartBar />
 
-  export const GearSixIcon = () =>{
-    return(
-      <GearSix/>  
-    )
-  }
-  export const SignOutIcon = () =>{
-    return(
-      <SignOut/>  
-    )
-  }
+
+export const ChartPieIcon = () => <ChartPie />
+
+
+export const ClipboardTextIcon = () => <ClipboardText />
+
+
+export const CreditCardIcon = () => <CreditCard />
+
+
+export const GearSixIcon = () => <GearSix />
+
+
+export const SignOutIcon = () => <SignOut />
--- client/src/ui/Icons/WalletIcons/WalletIcons.js
+++ client/src/ui/Icons/WalletIcons/WalletIcons.js
@@ -1,43 +1,24 @@
 import { ReactComponent as Edit } from './Icon-1.svg'
-import { ReactComponent as Recharge } from './Icon-4.svg'
-import { ReactComponent as Balance } from './Icon.svg'
-import { ReactComponent as Withdraw } from './Icon-3.svg'
 import { ReactComponent as Transactions } from './Icon-2.svg'
+import { ReactComponent as Withdraw } from './Icon-3.svg'
+import { ReactComponent as Recharge } from './Icon-4.svg'
 import { ReactComponent as Transfer } from './Icon-5.svg'
+import { ReactComponent as Balance } from './Icon.svg'
 
 
-export const EditIcon = () => {
-    return (
-        <Edit />
-    )
-}
+export const EditIcon = () => <Edit />
 
-export const RechargeIcon = () => {
-    return (
-        <Recharge />
-    )
-}
 
-export const BalanceIcon = () => {
-    return (
-        <Balance />
-    )
-}
+export const RechargeIcon = () => <Recharge />
 
-export const WithdrawIcon = () => {
-    return (
-        <Withdraw />
-    )
-}
 
-export const TransactionsIcon = () => {
-    return (
-        <Transactions />
-    )
-}
+export const BalanceIcon = () => <Balance />
 
-export const TransferIcon = () => {
-    return (
-        <Transfer />
-    )
-}
+
+export const WithdrawIcon = () => <Withdraw />
+
+
+export const TransactionsIcon = () => <Transactions />
+
+
+export const TransferIcon = () => <Transfer />
