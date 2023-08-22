b8b43328306e52143aaf1b1e0eb2b76cb118447f
Fedasevich <yurafedasevka@gmail.com>
2023-08-22 23:53:35 +0200

    * transfer wallet route

--- server/routes/walletRouter.js
+++ server/routes/walletRouter.js
@@ -5,6 +5,7 @@ const walletController = require('../controllers/walletController')
 const authMiddleware = require('../middleware/authMiddleware')
 
 router.post('/', authMiddleware, walletController.create)
+router.post('/transfer/from/:fromId/to/:toId', authMiddleware, walletController.transfer)
 router.get('/', authMiddleware, walletController.get)
 router.put('/:id', authMiddleware, walletController.change)
 router.delete('/:id', authMiddleware, walletController.delete)
