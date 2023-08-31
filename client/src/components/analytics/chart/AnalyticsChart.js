3a631517bf421949868b923086e044601958b115
Fedasevich <yurafedasevka@gmail.com>
2023-08-31 16:32:14 +0200

    * code split

--- client/src/components/analytics/chart/AnalyticsChart.js
+++ client/src/components/analytics/chart/AnalyticsChart.js
@@ -3,14 +3,12 @@ import { observer } from 'mobx-react-lite';
 import { useState } from 'react';
 import { Button } from 'react-bootstrap';
 import { Bar } from 'react-chartjs-2';
-import { useStore } from '../../../store';
-import { getCategoryBackgroundColorByIconId } from '../../../ui/Icons/CategoryIcons/CategoryIcons';
 import { ArrowDownIcon } from '../../../ui/Icons/ControlIcons/ControlIcons';
-import { getDateRangeOptions } from '../../../utils/constants';
+import { processChartData } from './libs/helpers/processChartData';
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip);
 
-export const options = {
+const options = {
   responsive: true,
   scales: {
     x: {
@@ -24,71 +22,20 @@ export const options = {
 };
 
 export const AnalyticsChart = observer(({ transactions, chartRange }) => {
-  const { category } = useStore();
-
   const [expanded, setExpanded] = useState(false);
 
   const handleExpandedToggle = () => {
     setExpanded((prev) => !prev);
   };
 
-  const processChartData = (data) => {
-    const chartData = {
-      labels: [],
-      datasets: []
-    };
-
-    const daysMap = new Map();
-
-    data.forEach((transaction) => {
-      const createdAt = new Date(transaction.createdAt);
-      const dateKey = getDateRangeOptions(createdAt, chartRange).print;
-
-      if (!daysMap.has(dateKey)) {
-        daysMap.set(dateKey, new Map());
-        chartData.labels.push(dateKey);
-      }
-
-      const { categoryId, description, sum, id } = transaction;
-
-      if (!daysMap.get(dateKey).has(id)) {
-        daysMap.get(dateKey).set(id, {
-          label: description,
-          data: [],
-          backgroundColor: getCategoryBackgroundColorByIconId(category.getIconIdFromCategoryById(categoryId))
-        });
-      }
-
-      daysMap.get(dateKey).get(id).data.push(Number(sum));
-    });
-
-    // eslint-disable-next-line no-restricted-syntax
-    for (const [dateKey, categoriesMap] of daysMap.entries()) {
-      // eslint-disable-next-line no-restricted-syntax
-      for (const [_, categoryData] of categoriesMap.entries()) {
-        if (!chartData.datasets.find((dataset) => dataset.label === categoryData.label)) {
-          chartData.datasets.push({
-            label: categoryData.label,
-            data: new Array(chartData.labels.length).fill(0),
-            backgroundColor: categoryData.backgroundColor
-          });
-        }
-        const dataIndex = chartData.labels.indexOf(dateKey);
-        chartData.datasets.find((dataset) => dataset.label === categoryData.label).data[dataIndex] +=
-          categoryData.data.reduce((total, value) => total + value, 0);
-      }
-    }
-
-    return chartData;
-  };
-
+  const processedData = processChartData(transactions, chartRange);
   return (
     <div className="analytics-chart-container">
-      <div className={`analytics-chart ${expanded ? 'expanded' : ''}`}>
-        <Bar options={options} data={processChartData(transactions)} />
+      <div className={`analytics-chart ${expanded ? 'expanded' : ''}`} data-testid="analytics-chart">
+        <Bar options={options} data={processedData} />
       </div>
       <div className="d-flex d-md-none justify-content-end expand-button">
-        <Button className="bg-light-blue border-0" onClick={handleExpandedToggle}>
+        <Button className="bg-light-blue border-0" onClick={handleExpandedToggle} data-testid="expand-button">
           <ArrowDownIcon />
         </Button>
       </div>
