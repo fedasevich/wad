2a6b69332f08cf0428bee74e9788e2f9782f30cb
Fedasevich <yurafedasevka@gmail.com>
2023-09-01 20:00:35 +0200

    * analytics expectedResult

--- client/src/components/analytics/statistics/__tests__/AnalyticsStatistics.test.js
+++ client/src/components/analytics/statistics/__tests__/AnalyticsStatistics.test.js
@@ -5,11 +5,15 @@ import { AnalyticsStatistics } from '../AnalyticsStatistics';
 
 const expectedResult = {
   dailyAverage: 10,
-  weeklyAverage: 20.0,
+  weeklyAverage: 30.0,
   monthlyAverage: 60.0
 };
 
 describe('AnalyticsStatistics.js', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('processes category statistics correctly', () => {
     render(<AnalyticsStatistics transactions={mockTransactions} />);
 
@@ -18,6 +22,7 @@ describe('AnalyticsStatistics.js', () => {
         name: /statistics/i
       })
     ).toBeInTheDocument();
+
     expect(screen.getByText(new RegExp(`Daily Average: ${expectedResult.dailyAverage}`, 'i'))).toBeInTheDocument();
     expect(screen.getByText(new RegExp(`Weekly Average: ${expectedResult.weeklyAverage}`, 'i'))).toBeInTheDocument();
     expect(screen.getByText(new RegExp(`Monthly Average: ${expectedResult.monthlyAverage}`, 'i'))).toBeInTheDocument();
