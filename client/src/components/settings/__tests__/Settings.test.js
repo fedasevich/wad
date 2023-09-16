b66ce089e4a77c81ebaaf4e34ba0ea9e3f76aab9
Fedasevich <yurafedasevka@gmail.com>
2023-09-16 21:09:36 +0200

    * store mock

--- client/src/components/settings/__tests__/Settings.test.js
+++ client/src/components/settings/__tests__/Settings.test.js
@@ -2,6 +2,8 @@ import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 import { Settings } from '../Settings';
 
+jest.mock('../../../store');
+
 describe('Settings.js', () => {
   it('renders without errors', () => {
     const { container } = render(<Settings />);
