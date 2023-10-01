51a632c63bcfb3282f363284b9e955550ebc811e
Fedasevich <yurafedasevka@gmail.com>
2023-10-01 18:42:28 +0200

    * exchange rates fetch

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,7 +1,11 @@
+import { isSameDay, parse } from 'date-fns';
+import { runInAction } from 'mobx';
 import { createContext, useContext } from 'react';
 import { fetchCategory } from '../http/categoryApi';
+import { fetchExchangeRates } from '../http/currencyApi';
 import { fetchWallet } from '../http/walletApi';
 import CategoryStore from './CategoryStore';
+import CurrencyStore from './CurrencyStore';
 import UserSettingsStore from './UserSettingsStore';
 import UserStore from './UserStore';
 import WalletStore from './WalletStore';
@@ -12,12 +16,25 @@ export class RootStore {
     this.user = new UserStore(this);
     this.category = new CategoryStore(this);
     this.wallet = new WalletStore(this);
+    this.currency = new CurrencyStore(this);
   }
 
   async initializeCategoriesAndWallets() {
     const [categoryData, walletData] = await Promise.all([fetchCategory(), fetchWallet()]);
     this.category.setCategories(categoryData);
     this.wallet.setWallet(walletData);
+
+    if (
+      !this.currency.exchangeRates.length ||
+      !isSameDay(new Date(), parse(this.currency.exchangeRatesDate, 'dd.MM.yyyy', new Date()))
+    ) {
+      const exchangeRates = await fetchExchangeRates();
+      this.currency.setExchangeRates(exchangeRates);
+    }
+
+    runInAction(() => {
+      this.currency.setUserCurrency();
+    });
   }
 }
 
@@ -28,7 +45,8 @@ export const StoreContext = createContext({
   category: rootStoreInstance.category,
   wallet: rootStoreInstance.wallet,
   userSettings: rootStoreInstance.userSettings,
-  rootStore: rootStoreInstance
+  rootStore: rootStoreInstance,
+  currency: rootStoreInstance.currency
 });
 
 export const useStore = () => useContext(StoreContext);
