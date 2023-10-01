cc03ba3218167f2fadca4f528de502fe60ced96f
Fedasevich <yurafedasevka@gmail.com>
2023-10-01 18:39:00 +0200

    * db models refactor, currency table

--- server/models/models.js
+++ server/models/models.js
@@ -3,29 +3,43 @@ const { DataTypes } = require('sequelize')
 
 const User = sequelize.define('user', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    email: { type: DataTypes.STRING, unique: true },
-    password: { type: DataTypes.STRING },
-    subscriber: { type: DataTypes.BOOLEAN, defaultValue: false }
+    email: { type: DataTypes.STRING, unique: true, allowNull: false },
+    password: { type: DataTypes.STRING, allowNull: false },
+})
+
+const Currency = sequelize.define('currency', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    currency: { type: DataTypes.STRING, allowNull: false },
+    code: { type: DataTypes.STRING, allowNull: false },
+    symbol: { type: DataTypes.STRING, allowNull: false },
 })
 
 const Category = sequelize.define('category', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     name: { type: DataTypes.STRING, allowNull: false },
-    spent: { type: DataTypes.INTEGER, defaultValue: 0 },
-    iconId: { type: DataTypes.INTEGER, defaultValue: 1 }
+    iconId: { type: DataTypes.INTEGER, defaultValue: 1, allowNull: false },
+    deletedAt: { type: DataTypes.DATE, allowNull: true, defaultValue: null }
+}, {
+    paranoid: true
 })
 
 const Transaction = sequelize.define('transaction', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     description: { type: DataTypes.STRING, allowNull: false },
-    sum: { type: DataTypes.DOUBLE, defaultValue: 0 },
-    walletId: { type: DataTypes.INTEGER, allowNull: false },
-    userId: { type: DataTypes.INTEGER, allowNull: false }
+    sum: { type: DataTypes.DOUBLE, defaultValue: 0, allowNull: false },
+    deletedAt: { type: DataTypes.DATE, allowNull: true, defaultValue: null }
+}, {
+    paranoid: true
 })
 
 
 const Wallet = sequelize.define('wallet', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     name: { type: DataTypes.STRING, allowNull: false },
-    balance: { type: DataTypes.DOUBLE, defaultValue: 0 },
-    currency: { type: DataTypes.STRING, allowNull: false }
+    balance: { type: DataTypes.DOUBLE, defaultValue: 0, allowNull: false },
+    deletedAt: { type: DataTypes.DATE, allowNull: true, defaultValue: null }
+}, {
+    paranoid: true
 })
 
 
@@ -36,11 +50,19 @@ Category.belongsTo(User)
 User.hasMany(Wallet)
 Wallet.belongsTo(User)
 
+User.belongsTo(Currency, {
+    foreignKey: {
+        name: 'currencyId',
+        allowNull: false,
+        defaultValue: 1,
+    },
+});
 
 Category.hasMany(Transaction)
 Transaction.belongsTo(Category)
-
+Transaction.belongsTo(User)
+Transaction.belongsTo(Wallet)
 
 module.exports = {
-    User, Category, Transaction, Wallet
+    User, Category, Transaction, Wallet, Currency
 }
