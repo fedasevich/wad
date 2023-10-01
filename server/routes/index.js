f62750dc2786dce97dbaf1954ff3f4540655f7c6
Fedasevich <yurafedasevka@gmail.com>
2023-10-01 18:39:54 +0200

    * currency path

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,11 +6,12 @@ const userRouter = require('./userRouter')
 const categoryRouter = require('./categoryRouter')
 const transactionRouter = require('./transactionRouter')
 const walletRouter = require('./walletRouter')
+const currencyRouter = require('./currencyRouter')
 
 router.use('/user', userRouter)
 router.use('/category', categoryRouter)
 router.use('/transaction', transactionRouter)
 router.use('/wallet', walletRouter)
-
+router.use('/currency', currencyRouter)
 
 module.exports = router;
