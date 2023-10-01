7379b3f05168b26991f05db5d80d88917dac89a2
Fedasevich <yurafedasevka@gmail.com>
2023-10-01 18:42:17 +0200

    * change currency id

--- client/src/store/UserStore.js
+++ client/src/store/UserStore.js
@@ -1,10 +1,13 @@
-import { makeAutoObservable } from 'mobx';
+import { makeAutoObservable, runInAction } from 'mobx';
+import toast from 'react-hot-toast';
+import { changeCurrencyId } from '../http/userApi';
 
 export default class UserStore {
   constructor(rootStore) {
     this.rootStore = rootStore;
     this._isAuth = false;
     this._user = {};
+
     makeAutoObservable(this);
   }
 
@@ -22,6 +25,28 @@ export default class UserStore {
 
   get user() {
     return this._user;
+  }
+
+  get currencyId() {
+    return this._user.currencyId;
+  }
+
+  changeCurrencyId(currencyId, newCurrencyRate, convert) {
+    try {
+      return changeCurrencyId(currencyId, newCurrencyRate, convert).then((data) => {
+        runInAction(() => {
+          this._user = data.user;
+          this.rootStore.currency.setExchangeRates(data.exchangeRates);
+          this.rootStore.currency.setUserCurrency();
+
+          if (convert) {
+            this.rootStore.wallet.setWallet(data.updatedWallets);
+          }
+        });
+      });
+    } catch (e) {
+      toast.error(e.response.data.message);
+    }
   }
 
   logOut() {
