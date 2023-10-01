21cb8ac05c6f86b9a7f9bcf08ca331e1fc67f5ec
Fedasevich <yurafedasevka@gmail.com>
2023-10-01 18:47:15 +0200

    * modal tags

--- client/src/components/calculator/CalculatorCategoryModal.js
+++ client/src/components/calculator/CalculatorCategoryModal.js
@@ -1,6 +1,5 @@
 import { observer } from 'mobx-react-lite';
 import React from 'react';
-import { Col, Row } from 'react-bootstrap';
 import { useStore } from '../../store';
 import { Icons } from '../../ui/Icons/CategoryIcons/CategoryIcons';
 import MenuProvider from '../MenuProvider';
@@ -19,22 +18,19 @@ const CalculatorCategoryModal = observer(({ categoryModalActive, setCategoryModa
         <h2>Choose category</h2>
       </MenuProvider.Header>
       <MenuProvider.Container>
-        <Row>
-          <Col md={12} className="overflow-auto vh-50 ">
-            {category.categories.map((categoryMap) => (
-              <button
-                type="button"
-                className="d-flex align-items-center justify-content-between w-100 text-start p-3 mb-2 modal_item"
-                onClick={() => handleCategorySelect(categoryMap)}
-                key={categoryMap.id}
-              >
-                <Icons iconId={categoryMap.iconId} />
-                <h3 className="mt-1">{categoryMap.name}</h3>
-                <h6>{categoryMap.spent}</h6>
-              </button>
-            ))}
-          </Col>
-        </Row>
+        <div className="overflow-auto vh-50 ">
+          {category.categories.map((categoryMap) => (
+            <button
+              type="button"
+              className="d-flex align-items-center justify-content-start w-100 text-start p-3 mb-2 modal_item"
+              onClick={() => handleCategorySelect(categoryMap)}
+              key={categoryMap.id}
+            >
+              <Icons iconId={categoryMap.iconId} />
+              <h3 className="ms-3">{categoryMap.name}</h3>
+            </button>
+          ))}
+        </div>
       </MenuProvider.Container>
     </Modal>
   );
