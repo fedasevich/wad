cb9fe56953bfb844a48aecd64356eb20413d669c
Fedasevich <yurafedasevka@gmail.com>
2023-10-21 19:53:34 +0200

    + calculator icons

---
+++ client/src/ui/Icons/CalculatorIcons/CalculatorIcons.js
@@ -0,0 +1,35 @@
+import { ReactComponent as Backspace } from './Backspace.svg';
+import { ReactComponent as CheckMark } from './CheckMark.svg';
+import { ReactComponent as Divide } from './Divide.svg';
+import { ReactComponent as Equals } from './Equals.svg';
+import { ReactComponent as Minus } from './Minus.svg';
+import { ReactComponent as Multiply } from './Multiply.svg';
+import { ReactComponent as Plus } from './Plus.svg';
+
+export function DivideIcon() {
+  return <Divide />;
+}
+
+export function EqualsIcon() {
+  return <Equals />;
+}
+
+export function MinusIcon() {
+  return <Minus />;
+}
+
+export function MultiplyIcon() {
+  return <Multiply />;
+}
+
+export function CheckMarkIcon() {
+  return <CheckMark />;
+}
+
+export function BackspaceIcon() {
+  return <Backspace />;
+}
+
+export function PlusIcon() {
+  return <Plus />;
+}
